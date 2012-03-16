11140193cf9304b98caa432af6b784cf934085d4
ikkebr <ikkibr@gmail.com>
2012-03-16 18:03:30 -0300

    Refatoração
    Refatorar

--- velha.py
+++ velha.py
@@ -1,5 +1,9 @@
 #*-* encoding: iso-8859-1 *-*
+
 from humano import Humano
+from robofacil import RoboFacil
+from robomedio import RoboMedio
+
 class Jogo(object):
 
     def __init__(self):
@@ -102,119 +106,9 @@ class Jogo(object):
                     print u"\tVencedor : %s" %self.vencedor
                 return
 
-class Humano(object):
-    '''Jogador Humano'''
-
-    def __init__(self,nome, marca):
-        self.nome = nome
-        self.marca = marca
-    
-    def jogada(self, tabuleiro):
-
-        while True:
-        
-            m = raw_input(u"Qual sua jogada? ")
-
-            try:
-                m = int(m)
-            except:
-                m = -1
-        
-            if m not in tabuleiro.movimentos_disponiveis():
-                print u"Movimento Inválido. Tente novamente."
-            else:
-                break
-    
-        tabuleiro.marcar(self.marca,m)
-
-class RoboFacil(object):
-    ''' Robo que joga aleatório '''
-    
-    def __init__(self, nome, marca):
-        self.nome = u"IA Fácil %s" % nome
-        self.marca = marca
-        
-        if self.marca == 'O':
-            self.oponente = 'X'
-        else:
-            self.oponente = 'O'
-    
-    def jogada(self, tabuleiro):
-        from random import choice
-        
-        tabuleiro.marcar( self.marca, choice( tabuleiro.movimentos_disponiveis() ) )
-        
-class Robo(object):
-    '''Robozinho'''
-    
-    def __init__(self, nome, marca):
-        self.nome = "IA %s" % nome
-        self.marca = marca
-        
-        self.inicio = 0
-        self.turno = 0
-        
-        if self.marca == 'O':
-            self.oponente = 'X'
-        else:
-            self.oponente = 'O'
-        
-    def jogada(self, tabuleiro):
-        self.turno += 1
-        
-        if tabuleiro.turno() == 1:
-            self.inicio = 1
-            
-        if self.inicio:
-            if self.turno == 1:
-                tabuleiro.marcar( self.marca, 4)
-                return True
-                
-            if self.turno == 2:
-                if self.oponente in [tabuleiro.tabuleiro[1], tabuleiro.tabuleiro[3], tabuleiro.tabuleiro[5], tabuleiro.tabuleiro[7]]:
-                    tabuleiro.marcar( self.marca, 2)
-                else:
-                    tabuleiro.marcar( self.marca, 1)
-                
-                return True
-                    
-            if self.turno >= 3:
-                if tabuleiro.pode_ganhar(self.marca):
-                   tabuleiro.marcar( self.marca, tabuleiro.pode_ganhar(self.marca))
-                elif tabuleiro.pode_ganhar(self.oponente):
-                   tabuleiro.marcar( self.marca, tabuleiro.pode_ganhar(self.oponente))
-                else:
-                   tabuleiro.marcar(self.marca, self.jogada_aleatoria(tabuleiro))   
-
-            return True   
-
-        else:
-            if self.turno == 1:
-              if tabuleiro.tabuleiro[4] == '-':
-                  self.inicio = 1
-                  tabuleiro.marcar(self.marca, 4)
-                  return True
-              else:
-                  tabuleiro.marcar(self.marca, 6)
-                  self.inicio = 1
-                  self.turno = 2
-                  return True
- 
-            
-        tabuleiro.marcar(self.marca, self.jogada_aleatoria(tabuleiro))
-                    
-            
-                    
-    def jogada_aleatoria(self, tabuleiro):
-        from random import choice
-        return choice(tabuleiro.movimentos_disponiveis())
-                    
-                    
-                                    
-
 if __name__ == '__main__':
     jogo=Jogo()     
     jogador2 = RoboFacil(u"Henrique", "X")
-    jogador1 = Robo(u"João", "O")
+    jogador1 = RoboMedio(u"João", "O")
     
     jogo.jogar( jogador2, jogador1)
